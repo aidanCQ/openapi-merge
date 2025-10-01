c0f060ed9cf0222ca3ea8665f6c8aae9dea3f74f
Aidan <104831665+aidanCQ@users.noreply.github.com>
2025-10-01 11:53:08 +0100

    Fix title.

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ import readSpecFromFile from './readSpecFromFile';
 R.pipe(
     process.argv.slice(2),
     R.map(readSpecFromFile),
-    specs => mergeSpecs(specs, 'my spec title'),
+    specs => mergeSpecs(specs, specs.map(({ info }) => info.title).join("+")),
     merged => JSON.stringify(merged, null, 2),
-    merged_as_json_string => process.stdout.write( merged_as_json_string),
+    merged_as_json_string => process.stdout.write(merged_as_json_string),
 )
